86860cceebfb37f1ee45907df3853168cb12252a
tostars <342795279@qq.com>
2022-05-05 16:52:23 +0800

    5.4work

--- src/com/towardstars/array/Demo02.java
+++ src/com/towardstars/array/Demo02.java
@@ -7,6 +7,10 @@ public class Demo02 {
             int[] a={1,2,3,4,5,6,7,8};
             System.out.println(a[0]);
 
+
+        for (int i = 0; i <  a.length-1;i++) {
+            System.out.println(a[i]);
+        }
             //动态初始化
             int[] b=new int[10];
             b[0]=10;
